0c92b742ab08938c61557d8aae6ea1089ed0d110
Konstantin Gorshkov <kostya_gorshkov_06@vk.com>
2025-12-05 18:40:08 +0300

    Created trait and basic function for EKU to trait ExtensionTrait

--- src/extensions/extended_key_usage.rs
+++ src/extensions/extended_key_usage.rs
@@ -32,3 +32,29 @@ impl ExtendedKeyUsage {
         self.ocsp_signing
     }
 }
+
+impl ExtensionTrait for ExtendedKeyUsage {
+    fn to_der(&self) -> Vec<u8> {
+        yasna::construct_der(|writer| {
+            writer.write_sequence(|seq| {
+                seq.next().write_bool(self.server_auth);
+                seq.next().write_bool(self.client_auth);
+                seq.next().write_bool(self.code_signing);
+                seq.next().write_bool(self.ocsp_signing);
+            });
+        })
+    }
+
+    fn from_der(der: &[u8]) -> Result<Self, Error> {
+        let result = yasna::parse_der(der, |reader| {
+            reader.read_sequence(|seq_read| {
+                let sa = seq_read.next().read_bool()?;
+                let clia = seq_read.next().read_bool()?;
+                let coda = seq_read.next().read_bool()?;
+                let oa = seq_read.next().read_bool()?;
+                Ok((sa, clia, coda, oa))
+            })
+        }).map_err(|e| Error::ASN1Error(crate::ASN1Wrapper(e)))?;
+        Ok( Self { server_auth: result.0, client_auth: result.1, code_signing: result.2, ocsp_signing: result.3 } )
+    }
+}
