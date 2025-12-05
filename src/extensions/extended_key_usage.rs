92ed024c76d46025ea27b44b7ad266ec65a5f89e
Konstantin Gorshkov <kostya_gorshkov_06@vk.com>
2025-12-05 18:30:56 +0300

    Added function for EKU: new

--- src/extensions/extended_key_usage.rs
+++ src/extensions/extended_key_usage.rs
@@ -11,3 +11,8 @@ pub struct ExtendedKeyUsage {
     ocsp_signing: bool,
 }
 
+impl ExtendedKeyUsage {
+    pub fn new(server_auth: bool, client_auth: bool, code_signing: bool, ocsp_signing: bool) -> Self {
+        Self { server_auth, client_auth, code_signing, ocsp_signing }
+    }
+}
