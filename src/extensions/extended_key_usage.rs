da31a13afe54fc8ec706eb653403801857a11a07
Konstantin Gorshkov <kostya_gorshkov_06@vk.com>
2025-12-05 18:33:07 +0300

    Added function for EKU: getters

--- src/extensions/extended_key_usage.rs
+++ src/extensions/extended_key_usage.rs
@@ -15,4 +15,20 @@ impl ExtendedKeyUsage {
     pub fn new(server_auth: bool, client_auth: bool, code_signing: bool, ocsp_signing: bool) -> Self {
         Self { server_auth, client_auth, code_signing, ocsp_signing }
     }
+
+    pub fn is_server_auth(&self) -> bool {
+        self.server_auth
+    }
+
+    pub fn is_client_auth(&self) -> bool {
+        self.client_auth
+    }
+
+    pub fn is_code_signing(&self) -> bool {
+        self.code_signing
+    }
+
+    pub fn is_ocsp_signing(&self) -> bool {
+        self.ocsp_signing
+    }
 }
