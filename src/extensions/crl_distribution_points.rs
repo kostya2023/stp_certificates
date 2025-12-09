8ac6e899596bce645c575593700c86d6d7c644a2
Konstantin Gorshkov <kostya_gorshkov_06@vk.com>
2025-12-09 16:10:50 +0300

    CRLDP base impl

--- src/extensions/crl_distribution_points.rs
+++ src/extensions/crl_distribution_points.rs
@@ -4,3 +4,17 @@ use crate::Error;
 use crate::extensions::ExtensionTrait;
 use yasna;
 
+pub struct CRLDistributionPoints {
+    uri: String,
+}
+
+impl CRLDistributionPoints {
+    pub fn new(uri: &str) -> Self {
+        Self { uri: uri.to_string() }
+    }
+
+    pub fn uri(&self) -> String {
+        self.uri.clone()
+    }
+}
+
