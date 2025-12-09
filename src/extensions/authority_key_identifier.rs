9a8a12dbde22ce5fe1d9745a387c8a0861baf918
Konstantin Gorshkov <kostya_gorshkov_06@vk.com>
2025-12-09 15:55:01 +0300

    AKI impl base

--- src/extensions/authority_key_identifier.rs
+++ src/extensions/authority_key_identifier.rs
@@ -10,3 +10,16 @@ pub struct AuthorityKeyIdentifier {
     key_identifier: Vec<u8>,
 }
 
+impl AuthorityKeyIdentifier {
+    pub fn new(hash_algorithm: ObjectIdentifier, key_identifier: Vec<u8>) -> Self {
+        Self { hash_algorithm, key_identifier }
+    }
+
+    pub fn hash_algorithm(&self) -> ObjectIdentifier {
+        self.hash_algorithm.clone()
+    }
+
+    pub fn key_identifier(&self) -> Vec<u8> {
+        self.key_identifier.clone()
+    }
+}
