8ee9f643086d2f82b456ff54afb70ec1e6298af0
Konstantin Gorshkov <kostya_gorshkov_06@vk.com>
2025-12-19 16:07:27 +0300

    Добавлена базовая структура

--- src/certs/certificate.rs
+++ src/certs/certificate.rs
@@ -1,3 +1,10 @@
 // certs/certificate.rs
 
-use crate::Error;
+use crate::{Error, certs::tbs_certificate::TbsCertificate, highlevel_keys::AlgorithmIdentifier};
+
+#[derive(Debug, Clone)]
+pub struct Certificate {
+    tbs_certificate: TbsCertificate,
+    signature_algorithm: AlgorithmIdentifier,
+    signature_value: Vec<u8>,
+}
