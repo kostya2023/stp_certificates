0315a6e168f0138c8131d557a5c50fe0a011e2c4
Konstantin Gorshkov <kostya_gorshkov_06@vk.com>
2025-12-19 16:12:38 +0300

    добавленный базовые геттеры

--- src/certs/certificate.rs
+++ src/certs/certificate.rs
@@ -8,3 +8,27 @@ pub struct Certificate {
     signature_algorithm: AlgorithmIdentifier,
     signature_value: Vec<u8>,
 }
+
+impl Certificate {
+    pub fn new(
+        tbs_certificate: TbsCertificate,
+        signature_algorithm: AlgorithmIdentifier,
+        signature_value: &[u8]
+    ) -> Self {
+        Self { tbs_certificate, signature_algorithm, signature_value: signature_value.to_vec() }
+    }
+
+    pub fn tbs_certificate(&self) -> TbsCertificate {
+        self.tbs_certificate.clone()
+    }
+
+    pub fn signature_algorithm(&self) -> AlgorithmIdentifier {
+        self.signature_algorithm.clone()
+    }
+
+    pub fn signature_value(&self) -> Vec<u8> {
+        self.signature_value.clone()
+    }
+}
+
+
